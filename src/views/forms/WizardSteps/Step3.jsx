e7bfb82d3af5ae889d0aef006c558c10dc73c44d
Alexander Bullock <alexanderbullock@Alexanders-Air.attlocal.net>
2019-11-18 04:17:37 -0500

    remove opt out

--- src/views/forms/WizardSteps/Step3.jsx
+++ src/views/forms/WizardSteps/Step3.jsx
@@ -67,7 +67,7 @@ class CreateAppointment extends React.Component {
       message += `I scheduled your VIP appointment at ${data.appointment.dealership.label} located at ${data.appointment.dealership.address} for `
       let tempDate = new Date(data.appointment.date)
       message += tempDate.toLocaleDateString()+ " @ " + tempDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) + ". "
-      message += "We are excited to assist you! Please ask for the VIP manager at the receptionist desk.\n\nReply STOP to stop receiving messages"
+      message += "We are excited to assist you! Please ask for the VIP manager at the receptionist desk."
       return message
     }
   }
