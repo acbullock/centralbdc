748179e2c77d63f664448581571887c5823f961c
Alexander Bullock <alexanderbullock@Alexanders-MacBook-Air.local>
2019-12-31 16:35:02 -0500

    dealer goals tweaks

--- src/views/pages/DealershipPerformance.jsx
+++ src/views/pages/DealershipPerformance.jsx
@@ -40,16 +40,16 @@ class DealershipPerformance extends React.Component {
         // let appointments = this._isMounted && await this.props.mongo.find("appointments");
         let agents = this._isMounted && await this.props.mongo.find("agents");
         // this.setState({appointments})
-        this.setState({agents})
-        dealerships = dealerships.filter((d)=>{return d.isActive === true})
+        this.setState({ agents })
+        dealerships = dealerships.filter((d) => { return d.isActive === true })
         dealerships.sort((a, b) => {
             if (parseInt(a.goal) > parseInt(b.goal)) return -1;
             if (parseInt(a.goal) < parseInt(b.goal)) return 1;
             return 0;
         })
-        this.setState({dealerships})
-        
-        for(let d in dealerships){
+        this.setState({ dealerships })
+
+        for (let d in dealerships) {
             this._isMounted && await this.todayDealerCount(dealerships[d])
         }
         console.log(new Date())
@@ -63,20 +63,50 @@ class DealershipPerformance extends React.Component {
     }
     async todayDealerCount(dealership) {
         let apps = [];
-        for(let a in this.state.agents){
-            for(let b in this.state.agents[a].appointments){
-                if(this.state.agents[a].appointments[b].dealership.value === dealership.value){
+        for (let a in this.state.agents) {
+            for (let b in this.state.agents[a].appointments) {
+                if (this.state.agents[a].appointments[b].dealership.value === dealership.value) {
                     apps.push(this.state.agents[a].appointments[b])
                 }
             }
         }
         let dlr = this.state.dealerships.map((d) => {
             if (d.value === dealership.value) {
-                return Object.assign(d, { totalCount: apps.length })
+                let progressValue = Math.round(apps.length * 10 / d.goal * 100) / 10;
+                let progressColor = "red";
+                if (progressValue > 33) {
+                    progressColor = "yellow"
+                }
+                if (progressValue >= 100) {
+                    progressColor = "green"
+                }
+                let todayStart = new Date()
+                todayStart.setHours(8, 0, 0, 0)
+
+                let now = new Date();
+
+                let elapsed = now.getTime() - todayStart.getTime()
+                //if before 8am..
+                if (elapsed < 0) {
+                    elapsed = 0;
+                }
+                let hourlyRate = elapsed / (1000 * 60 * 60)
+                hourlyRate = apps.length / hourlyRate
+                let totalHrs = now.getDay() === 0 ? 8 : 12;
+                let projection = Math.round(10* hourlyRate * totalHrs)/10
+
+
+
+                return Object.assign(d, { totalCount: apps.length, progressValue, progressColor, projection: projection })
             }
             else {
                 return d;
             }
+        })
+        dlr.sort((a, b) => {
+            if (a.progressValue > b.progressValue) return 1;
+            if (a.progressValue < b.progressValue) return -1;
+            return 0;
         })
         this.setState({ dealerships: dlr });
     }
@@ -110,25 +140,18 @@ class DealershipPerformance extends React.Component {
                                             <th>Dealership Name</th>
                                             <th>Today's Appointment Count</th>
                                             <th>Dealership Goal</th>
+                                            <th>On Track</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         {this.state.dealerships.map((d, i) => {
-                                            let percent  = d.totalCount/d.goal * 100;
-                                            let red = 33;
-                                            let color = "red"
-                                            if(percent > red){
-                                                color = "yellow"
-                                            }
-                                            if(percent >= 100){
-                                                color="green"
-                                            }
                                             return (
                                                 <tr key={i}>
-                                                    <td><Progress value={percent} color={color}/></td>
+                                                    <td><Progress value={d.progressValue} color={d.progressColor}><strong>{d.progressValue}%</strong></Progress></td>
                                                     <td>{d.label}</td>
                                                     <td>{d.totalCount}</td>
                                                     <td>{d.goal}</td>
+                                            <td><i style={{color:d.projection >= d.goal ? "green": "red"}} className={d.projection >= d.goal ? "tim-icons icon-check-2": "tim-icons icon-simple-remove"}/></td>
                                                 </tr>
                                             );
 
