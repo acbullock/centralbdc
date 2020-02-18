2cc262e0c8562ee92f7602edd5122e68b0b85d0c
Alexander Bullock <alexanderbullock@Alexanders-MacBook-Air.local>
2020-02-18 14:28:20 -0500

    fix dash crash

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -105,7 +105,7 @@ class Dashboard extends React.Component {
       agents = this._isMounted && agents.map((a, i) => {
         return Object.assign(a, { label: a.name, value: i })
       })
-      agents.sort((a,b)=>{
+      this._isMounted && agents.sort((a,b)=>{
         return b.appointments.length - a.appointments.length
       })
       // agents = this._isMounted && agents.filter((a) => {
